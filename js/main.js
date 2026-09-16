bbd84ee66648416e2a86ae322c2ff6aced23ba95
Ilia Faramarzpour <iliafaramarzpour@gmail.com>
2026-09-16 17:36:34 +0330

    fix(content): fix html structure and style - add counter for discounted products

--- js/main.js
+++ js/main.js
@@ -23,3 +23,33 @@ var discountedProductsSlider = new Swiper(".discounted_products_swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+let _second = 1000;
+let _minute = _second * 60;
+let _hour = _minute * 60;
+let _day = _hour * 24;
+let timer = 24 * 60 * 60;
+
+function showRemaining() {
+  const now = Date.now();
+
+  const end = new Date();
+
+  end.setHours(24, 0, 0, 0);
+
+  const remaining = end - now;
+
+  const hours = Math.floor(remaining / 1000 / 60 / 60);
+  const minutes = Math.floor((remaining % _hour) / _minute);
+  const seconds = Math.floor((remaining % _minute) / _second);
+
+  const secondsElement = document.querySelector("#second");
+  const minutesElement = document.querySelector("#minutes");
+  const hoursElement = document.querySelector("#hours");
+
+  hoursElement.textContent = hours < 10 ? "0" + hours : hours;
+  minutesElement.textContent = minutes < 10 ? "0" + minutes : minutes;
+  secondsElement.textContent = seconds < 10 ? "0" + seconds : seconds;
+}
+
+setInterval(showRemaining, 1000);
